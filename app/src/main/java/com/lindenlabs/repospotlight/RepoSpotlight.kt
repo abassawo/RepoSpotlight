299c72f5086119080f642d14c7b787dcc2007dca
Abass Bayo <abass.bayo@gmail.com>
2024-05-14 21:34:53 -0400

    init commit - add required dependencies

---
+++ app/src/main/java/com/lindenlabs/repospotlight/RepoSpotlight.kt
@@ -0,0 +1,11 @@
+package com.lindenlabs.repospotlight
+
+import android.app.Application
+
+class RepoSpotlight : Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+
+    }
+}
---
+++ build.gradle.kts
@@ -0,0 +1,13 @@
+buildscript {
+    dependencies {
+        classpath("com.google.android.libraries.mapsplatform.secrets-gradle-plugin:secrets-gradle-plugin:2.0.1")
+    }
+}
+
+
+plugins {
+    alias(libs.plugins.androidApplication) apply false
+    alias(libs.plugins.jetbrainsKotlinAndroid) apply false
+    id("com.google.dagger.hilt.android") version "2.44" apply false
+    id("com.google.android.libraries.mapsplatform.secrets-gradle-plugin") version("2.0.1") apply false
+}
