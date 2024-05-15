f1b223962a457c52dbdec77ffcc22afa92df50ca
Abass Bayo <abass.bayo@gmail.com>
2024-05-15 08:40:03 -0400

    WIP - fetching up to 100 repositories

--- app/src/main/java/com/lindenlabs/repospotlight/data/AppRepository.kt
+++ app/src/main/java/com/lindenlabs/repospotlight/data/AppRepository.kt
@@ -5,23 +5,21 @@ import com.lindenlabs.repospotlight.data.models.RepoModel
 import okhttp3.OkHttpClient
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
-import javax.inject.Inject
 
 interface AppDataSource {
     suspend fun getPopularRepos(): List<RepoModel>
 }
 
-class AppRepository @Inject constructor() : AppDataSource {
+class AppRepository : AppDataSource {
     override suspend fun getPopularRepos(): List<RepoModel> {
         return api.popularRepos().items
     }
 
     companion object {
-        val BASE_URL = "https://api.github.com"
-        val api = Retrofit.Builder().baseUrl(BASE_URL)
+        private val BASE_URL = "https://api.github.com"
+        private val api = Retrofit.Builder().baseUrl(BASE_URL)
             .client(OkHttpClient())
             .addConverterFactory(GsonConverterFactory.create(Gson()))
             .build().create(RestApi::class.java)
     }
-
 }
--- app/src/main/java/com/lindenlabs/repospotlight/data/RestApi.kt
+++ app/src/main/java/com/lindenlabs/repospotlight/data/RestApi.kt
@@ -2,8 +2,15 @@ package com.lindenlabs.repospotlight.data
 
 import com.lindenlabs.repospotlight.data.models.RawSpotlightResponse
 import retrofit2.http.GET
+import retrofit2.http.Query
 
 interface RestApi {
+//    @GET("/search/repositories?q=stars:>0")
+//    suspend fun popularRepos(): RawSpotlightResponse
+
     @GET("/search/repositories?q=stars:>0")
-    suspend fun popularRepos(): RawSpotlightResponse
+    suspend fun popularRepos(
+        @Query("per_page") pageSize: Int = 100,
+        @Query("page") page: Int = 1
+    ): RawSpotlightResponse
 }
